b768f038a2a596d4244c7682cf9d009d678a356a
Rodrigo Bernardi <rodrigo.n.bernardi@gmail.com>
2019-07-31 14:43:55 -0300

    install vscode and extensions added

---
+++ vscode-install.sh
@@ -0,0 +1,68 @@
+cd ~/Downloads/
+
+sudo apt update
+sudo apt install wget
+wget https://go.microsoft.com/fwlink/?LinkID=760868
+sudo apt install ./code_*.deb
+
+
+code --install-extension abusaidm.html-snippets
+code --install-extension akamud.vscode-theme-onedark
+code --install-extension alefragnani.Bookmarks
+code --install-extension bpruitt-goddard.mermaid-markdown-syntax-highlighting
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension ExodiusStudios.comment-anchors
+code --install-extension frhtylcn.pythonsnippets
+code --install-extension GrapeCity.gc-excelviewer
+code --install-extension hdg.live-html-previewer
+code --install-extension hsnazar.hyper-term-theme
+code --install-extension joker-theme.joker
+code --install-extension karyfoundation.comment
+code --install-extension kevinkyang.auto-comment-blocks
+code --install-extension mikestead.dotenv
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode.Theme-1337
+code --install-extension octref.vetur
+code --install-extension oguhpereira.spotify-color-theme
+code --install-extension PKief.material-icon-theme
+code --install-extension redhat.vscode-yaml
+code --install-extension ritwickdey.live-sass
+code --install-extension ritwickdey.LiveServer
+code --install-extension sdras.vue-vscode-snippets
+code --install-extension tomoki1207.pdf
+code --install-extension voldemortensen.rainbow-tags
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension vstirbu.vscode-mermaid-preview
+code --install-extension xabikos.JavaScriptSnippets
+code --install-extension Zignd.html-css-class-completion
+
+
+# code --install-extension abusaidm.html-snippets
+# code --install-extension akamud.vscode-theme-onedark
+# code --install-extension alefragnani.Bookmarks
+# code --install-extension christian-kohler.npm-intellisense
+# code --install-extension CoenraadS.bracket-pair-colorizer
+# code --install-extension dbaeumer.vscode-eslint
+# code --install-extension frhtylcn.pythonsnippets
+# code --install-extension GrapeCity.gc-excelviewer
+# code --install-extension Gruntfuggly.todo-tree
+# code --install-extension hdg.live-html-previewer
+# code --install-extension hollowtree.vue-snippets
+# code --install-extension joker-theme.joker
+# code --install-extension lkytal.pomodoro
+# code --install-extension mikestead.dotenv
+# code --install-extension ms-vscode.Theme-1337
+# code --install-extension octref.vetur
+# code --install-extension oguhpereira.spotify-color-theme
+# code --install-extension PKief.material-icon-theme
+# code --install-extension ritwickdey.LiveServer
+# code --install-extension tomoki1207.pdf
+# code --install-extension voldemortensen.rainbow-tags
+# code --install-extension vscode-icons-team.vscode-icons
+# code --install-extension xabikos.JavaScriptSnippets
+# code --install-extension Zignd.html-css-class-completion
+
+
+
